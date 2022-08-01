a8da8bc0cb9373b7e844fbaae7f266836234f09e
abeneby <49881141+abeneby@users.noreply.github.com>
2022-08-01 11:43:27 -0500

    adding project

---
+++ src/main/java/com/cogent/NumbeCalculation.java
@@ -0,0 +1,20 @@
+package com.cogent;
+
+public class NumbeCalculation {
+	public int add(int x, int y) {
+		return x+y; 
+	}
+	
+	public int subtract(int x, int y) {
+		return x-y; 
+	}
+	
+	public int multiply(int x, int y) {
+		return x*y; 
+	}
+	
+	public int divide(int x, int y) {
+		return x/y; 
+	}
+
+}
---
+++ src/test/java/com/cogent/NumberCalculationTest.java
@@ -0,0 +1,56 @@
+package com.cogent;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class NumberCalculationTest {
+	NumbeCalculation nc; 
+	
+	@BeforeEach
+	void init() {
+		nc=new NumbeCalculation(); 
+	}
+	
+	@Test
+	void testAddition() {
+		assertEquals(nc.add(10, 20), 30); 
+	}
+	
+	@Nested
+	class AddTest{
+		@Test
+		void AddPositive() {
+			assertEquals(2, nc.add(1, 1)); 
+			
+		}
+		@Test
+		void addPositive_Negative() {
+			assertEquals(0, nc.add(1, -1));
+			
+		}
+		@Test
+		void AddNegative() {
+			assertEquals(-2, nc.add(-1, -1));
+			
+		}
+	}
+	@Test
+	void testMultiply() {
+		assertAll(
+				()->assertEquals(0,nc.multiply(1,0)), 
+				()->assertEquals(7,nc.multiply(7,1)), 
+				()->assertEquals(20,nc.multiply(4,5))
+				
+				); 
+	}
+	
+	@Test
+	void testDivide() {
+		assertThrows(ArithmeticException.class, ()->nc.divide(1,0), "denominator should not be zero"); 
+	}
+}
